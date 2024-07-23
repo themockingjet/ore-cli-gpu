c598cac7309ddd1824293f2189625ad2a936da36
themockingjet <jwoot001@gmail.com>
2024-07-23 09:38:01 +0800

    siphash

--- cuda/hashx/src/siphash.cu
+++ cuda/hashx/src/siphash.cu
@@ -6,61 +6,63 @@
 #include "unreachable.h"
 
 uint64_t hashx_siphash13_ctr(uint64_t input, const siphash_state* keys) {
-    uint64_t v0 = keys->v0;
-    uint64_t v1 = keys->v1;
-    uint64_t v2 = keys->v2;
-    uint64_t v3 = keys->v3;
+  uint64_t v0 = keys->v0;
+  uint64_t v1 = keys->v1;
+  uint64_t v2 = keys->v2;
+  uint64_t v3 = keys->v3;
 
-    v3 ^= input;
+  v3 ^= input;
 
-    SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
 
-    v0 ^= input;
-    v2 ^= 0xff;
+  v0 ^= input;
+  v2 ^= 0xff;
 
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
 
-    return (v0 ^ v1) ^ (v2 ^ v3);
+  return (v0 ^ v1) ^ (v2 ^ v3);
 }
 
 __device__ void hashx_siphash24_ctr_state512(const siphash_state* keys, uint64_t input,
-    uint64_t state_out[8]) {
+                                             uint64_t state_out[8]) {
+  uint64_t v0 = keys->v0;
+  uint64_t v1 = keys->v1;
+  uint64_t v2 = keys->v2;
+  uint64_t v3 = keys->v3;
 
-    uint64_t v0 = keys->v0;
-    uint64_t v1 = keys->v1;
-    uint64_t v2 = keys->v2;
-    uint64_t v3 = keys->v3;
+  const uint64_t c1 = 0xee;
+  const uint64_t c2 = 0xdd;
 
-    v1 ^= 0xee;
-    v3 ^= input;
+  v1 ^= c1;
+  v3 ^= input;
 
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
 
-    v0 ^= input;
-    v2 ^= 0xee;
+  v0 ^= input;
+  v2 ^= c1;
 
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
 
-    state_out[0] = v0;
-    state_out[1] = v1;
-    state_out[2] = v2;
-    state_out[3] = v3;
+  state_out[0] = v0;
+  state_out[1] = v1;
+  state_out[2] = v2;
+  state_out[3] = v3;
 
-    v1 ^= 0xdd;
+  v1 ^= c2;
 
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
-    SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
+  SIPROUND(v0, v1, v2, v3);
 
-    state_out[4] = v0;
-    state_out[5] = v1;
-    state_out[6] = v2;
-    state_out[7] = v3;
+  state_out[4] = v0;
+  state_out[5] = v1;
+  state_out[6] = v2;
+  state_out[7] = v3;
 }
